614763d606a79c6a3f5860ec1d2a8d039e510d01
stein <amirashurov11@gmail.com>
2016-05-01 15:44:15 +0500

    Сделал асинхроный вход и выход

--- MVC/ViewModels/UserViewModel.cs
+++ MVC/ViewModels/UserViewModel.cs
@@ -1,11 +1,23 @@
 ﻿using System;
 using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
+using MVC.Models;
 
 namespace MVC.ViewModels
 {
     public class UserViewModel
     {
+
+        public UserViewModel(User user)
+        {
+            if (user == null)return;
+            Login = user.LoginName;
+        }
+
+        public UserViewModel()
+        {
+            
+        }
         [Required]
         public String Login { get; set; }
         [Required]
